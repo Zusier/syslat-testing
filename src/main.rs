fd7ed846e0f3ff0d183bd16bbebc7242907fbbbc
Zusier <zusier@pm.me>
2022-02-24 05:16:38 -0600

    use json for results_count/total

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
-use std::{
-    fs::File,
-    io::{BufReader},
-};
 use serde_json::Value;
+use std::{fs::File, io::BufReader};
 
 fn main() {
     // .json found at ./data.json
     let file = File::open("test_data.json").unwrap();
     let reader = BufReader::new(file);
-
-    // Todo: use counts/total from json
-    // total ms in the file
-    let mut results_total: u64 = 0;
-    // how many data points are in the file
-    let mut results_count: u64 = 0;
-    let mut results_vec: Vec<u64> = Vec::new();
-
     let json: Value = serde_json::from_reader(reader).unwrap();
 
-    for entry in json["SysLatData"].as_object().unwrap()["SysLatResults"]
-        .as_array()
-        .unwrap()
-    {
+    // Todo: use counts/total from json
+    let mut results_vec: Vec<u64> = Vec::new();
+
+    let results_count = json["AggregateData"].as_object().unwrap().get("SysLatTestCount").unwrap().as_u64().unwrap();
+    let results_total = json["AggregateData"].as_object().unwrap().get("SystemLatencyTotal").unwrap().as_u64().unwrap();
+
+    for entry in json["SysLatData"].as_object().unwrap()["SysLatResults"].as_array().unwrap() {
         //println!("{:?}", entry.as_u64().unwrap());
-        results_total += entry.as_u64().unwrap();
-        results_count += 1;
         results_vec.push(entry.as_u64().unwrap());
     }
     results_vec.sort();
